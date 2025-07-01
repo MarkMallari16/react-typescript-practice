50dae2ed5dc87b032a71a4dc5137206ef29df847
Mark Christian Mallari <mallariftw01@gmail.com>
2025-07-01 22:25:43 +0800

    upd: add edit list

--- src/App.tsx
+++ src/App.tsx
@@ -3,44 +3,97 @@ import React, { useState } from 'react'
 interface Lists {
   id: number,
   title: string,
+  isEdit: boolean
 }
+
 const App: React.FC = () => {
+  //title input
   const [title, setTitle] = useState<string>("");
+  const [newTitle, setNewTitle] = useState<string>("");
+
+  //storing list
   const [lists, setLists] = useState<Lists[]>([]);
 
-  const handleTitle = (e: string) => {
-    setTitle(e);
+  //handle title for title iinput 
+  const handleTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
   }
+
+  //add 
   const addList = () => {
+    if (title.trim() === "") {
+      alert("Please input field");
+      return;
+    }
+
     const newList: Lists = {
       id: lists.length + 1,
-      title: title
+      title: title,
+      isEdit: false
     };
+
     setLists((prevlists) => [...prevlists, newList]);
     setTitle("");
-
   }
 
+  //delete
   const deleteList = (id: number) => {
     setLists((prevLists) => prevLists.filter(list => list.id !== id))
   }
 
+  //edit
+  const editList = (id: number) => {
+    const editingItem = lists.find((l) => l.id === id);
+
+    if (editingItem) {
+      setNewTitle(editingItem.title);
+    }
+
+    setLists((prevLists) => prevLists.map((list) => (
+      list.id === id ? { ...list, isEdit: true } : list
+    )))
+  }
+
+  //save
+  const saveList = (id: number) => {
+    setLists((prevlists) => prevlists.map((list) => (
+      list.id === id ? { ...list, title: newTitle, isEdit: false } : list
+    )))
+  }
+  const cancelEdit = (id: number) => {
+    setLists((prevLists) => prevLists.map((list) => (
+      list.id === id ? { ...list, isEdit: false } : list
+    )))
+  }
   return (
     <>
       <h1>Todo List Application</h1>
       <div>
         <label htmlFor="title">Title</label>
-        <input value={title} onChange={(e) => handleTitle(e.target.value)} id='title' type="text" />
+        <input value={title} onChange={(e) => handleTitle(e)} id='title' type="text" />
       </div>
       <button onClick={addList}>Add List</button>
 
       <div>
         {
           lists.map((list) => (
-            <>
-              <div>{list.title}</div>
-              <button onClick={() => deleteList(list.id)}>Delete</button>
-            </>
+            <div key={list.id}>
+              {list.isEdit ?
+                <>
+                  <input value={newTitle} onChange={(e) => setNewTitle(e.target.value)}
+                    id='title' type="text" />
+                  <button onClick={() => cancelEdit(list.id)}>Cancel</button>
+                  <button onClick={() => saveList(list.id)}>Save</button>
+
+                </>
+                :
+                <>
+                  <div>{list.title}</div>
+                  <button onClick={() => editList(list.id)}>Edit</button>
+                  <button onClick={() => deleteList(list.id)}>Delete</button>
+                </>
+              }
+            </div>
           ))
         }
       </div>
