f8642711fea2bb842aaa83d1acc2e49e4af56714
Mark Christian Mallari <mallariftw01@gmail.com>
2025-07-03 00:17:52 +0800

    add: props and Button Component

--- src/component/Button.tsx
+++ src/component/Button.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 interface MyButtonProps {
     title: string,
-    disabled: boolean
+    disabled: boolean,
 }
 const Button: React.FC<MyButtonProps> = ({ title, disabled }) => {
     return (
-        <button disabled={disabled} >{title}</button>
+        <button disabled={disabled} className='text-xl bg-slate-500 hover:bg-slate-400 transition-all ease-in-out px-2 py-1 rounded-md mt-2 text-white cursor-pointer'>{title}</button>
     )
 }
 
